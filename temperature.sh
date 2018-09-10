9a01d9cf9d7e1f64d367e4a48e73d4e985bf0705
Ethan Hammond <ehammond@ksu.edu>
2018-09-09 19:53:08 -0500

    Added Kelvin to output

--- temperature.sh
+++ temperature.sh
@@ -2,8 +2,7 @@
 
 echo "Enter a temperature in F to convert to C: "
 read temp
+echo "Temperature in Celsius:"
 echo 'scale=3;'"5/9*($temp-32)" |bc
-
-
-
-
+echo "Temperature in Kelvin:"
+echo 'scale=3;'"5/9*($temp-32)+273.15" |bc
